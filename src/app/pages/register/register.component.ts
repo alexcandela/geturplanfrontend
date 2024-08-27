f6f4dac3077f2b63734ced765bf0c2d5a22c5258
Alex Candela <alex.candelaa@gmail.com>
2024-08-27 23:08:23 +0200

    Feedback de contraseña + animación de barra para mostrar al usuario cuanto de segura es la contraseña + modificado modo mobile de publicar y editar plan.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -10,11 +10,12 @@ import { Title } from '@angular/platform-browser';
 import { Router } from '@angular/router';
 import { RegisterService } from '../../core/services/register.service';
 import { Register } from '../../core/interfaces/register';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
@@ -28,6 +29,10 @@ export class RegisterComponent {
   showPasswordImg: WritableSignal<string> = signal(this.eye);
   usernameBackend = signal(false);
   emailBackend = signal(false);
+  show = signal(false);
+  nonsecure = signal(false);
+  mediumsecure = signal(false);
+  secure = signal(false);
 
   constructor(
     private fb: FormBuilder,
@@ -41,10 +46,17 @@ export class RegisterComponent {
       apellido: ['', [Validators.required]],
       username: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [
-        Validators.required,
-        Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/)
-      ]]      
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/),
+        ],
+      ],
+    });
+
+    this.registerForm.get('password')?.valueChanges.subscribe((value) => {
+      this.passbar(value);
     });
   }
 
@@ -58,11 +70,52 @@ export class RegisterComponent {
       : this.showPasswordImg.set(this.dashedeye);
   };
 
+  showPasswordFeedback = () => {
+    this.show.set(true);
+  };
+
+  hidePasswordFeedback = () => {
+    this.show.set(false);
+  };
+
+  getPassbarClass() {
+    return {
+      'secure': this.secure(),
+      'mediumsecure': this.mediumsecure(),
+      'nonsecure': this.nonsecure()
+    };
+  }
+
+  passbar = (passwordValue: string): void => {
+    if (passwordValue.length === 0) {
+      this.nonsecure.set(false);
+      this.mediumsecure.set(false);
+      this.secure.set(false);
+      this.show.set(true);
+    } else if (passwordValue.length < 8) {
+      this.nonsecure.set(true);
+      this.mediumsecure.set(false);
+      this.secure.set(false);
+      this.show.set(true);
+    } else if (passwordValue.length >= 8 && !/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/.test(passwordValue)) {
+      this.nonsecure.set(false);
+      this.mediumsecure.set(true);
+      this.secure.set(false);
+      this.show.set(true);
+    } else if (passwordValue.length >= 8 && /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/.test(passwordValue)) {
+      this.nonsecure.set(false);
+      this.mediumsecure.set(false);
+      this.secure.set(true);
+      this.show.set(false);
+    }
+    
+  };
+
   reset = () => {
     this.submitted.set(false);
     this.emailBackend.set(false);
     this.usernameBackend.set(false);
-  }
+  };
 
   onSubmit = (registerData: Register) => {
     this.reset();
